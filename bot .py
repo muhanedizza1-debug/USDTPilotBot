c2792f9296921f66fd3abb86dcc5ae2c95418b96
muhanedizza1-debug <muhanedizza1@gmail.com>
2026-08-03 22:33:23 +0300

    Update and rename bot .pay to bot .py

--- bot .py
+++ bot .py
@@ -24,6 +24,7 @@ def echo(message):
 
 if __name__ == "__main__":
     thread = threading.Thread(target=bot.infinity_polling)
+    thread.daemon = True
     thread.start()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
